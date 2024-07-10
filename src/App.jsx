185793e99bb6b5882fe04e2a045e024654ff0d29
Silvina Figueroa <silvina.sfigueroa@gmail.com>
2024-07-10 15:03:55 -0700

    implementation

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+// Imports
+import { useState } from "react"
+import axios from "axios"
+
+//Components
+import Form from './components/Form'
+import MovieDisplay from './components/MovieDisplay'
+
+let apiKey = import.meta.env.VITE_apiKey
 
 function App() {
-  const [count, setCount] = useState(0)
+  //UseState
+  const [movieData, setMovieData] = useState(null)
+
+  async function search(searchTerm) {
+    try {
+      // Make and apiCall and save response in variable
+      let response = await axios.get(`http://www.omdbapi.com/?apikey=${apiKey}&t=${searchTerm}`)
+      setMovieData(response.data)
+
+    } catch (err) {
+      console.error(err)
+
+    }
+
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Form movieSearch={search} />
+      <MovieDisplay />
     </>
   )
 }
